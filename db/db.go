c2fe5a7f0b2244a695a134259dc48e7f3b02272d
Andrew Doherty <adohert2@illinois.edu>
2018-04-08 16:29:16 -0500

    Added support for three tables and moved constants to another file

--- db/db.go
+++ db/db.go
@@ -5,22 +5,31 @@ import (
 	"bytes"
 	"database/sql"
 	"fmt"
+	"log"
 )
 
-
-var hostnameStart = "tcp(127.0.0.1:" // CONSTANT
-var hostnameEnd = ")/" //CONSTANT
-var username = "root"
-var databaseName = "OTRMessenger" // Constant in execution, can change
-var tableName = "Messages" // Constant in execution, can change
-
-var password = "" // may change for each user
-var port = "3306" // may change for each user
+const (
+	hostnameStart = "tcp(127.0.0.1:" // CONSTANT
+	hostnameEnd = ")/" //CONSTANT
+	databaseName = "otrmessenger" // Constant in execution, can change
+	sessionsTableName = "sessions"
+	userTableName = "users"
+	conversationTableName = "conversation"
+	// tableName = "Messages" // Constant in execution, can change
+)
 
 // Function to be called to set everything up
-func SetupDatabase() {
+func SetupDatabase() *sql.DB {
 	db := InitializeDatabase()
-	SetupMessagesTable(db)
+	SetupTables(db)
+	SetupSessionsTable(db)
+	return db
+}
+func SetupTables(db *sql.DB) {
+	SetupSessionsTable(db)
+	SetupUsersTable(db)
+	SetupConversationTable(db)
+
 }
 
 // Drops the database if it exists
@@ -87,24 +96,108 @@ func FormConnectionString(Name string) string {
 	return connectionString.String()
 }
 
-// Creates the table schema
-func SetupMessagesTable(db *sql.DB) {
+// Creates the sessions table
+func SetupSessionsTable(db *sql.DB) {
 	var createTableCommand bytes.Buffer
 	createTableCommand.WriteString("CREATE TABLE IF NOT EXISTS ")
-	createTableCommand.WriteString(tableName)
+	createTableCommand.WriteString(sessionsTableName)
 	createTableCommand.WriteString(" (\n")
-	//TODO: Update the db schema
-	createTableCommand.WriteString("username char(50) NOT NULL, \n")
-	createTableCommand.WriteString("friendName char(50) NOT NULL, \n")
-	createTableCommand.WriteString("personalPrivateKey char(50) NOT NULL, \n")
-	createTableCommand.WriteString("friendPublicKey char(50) NOT NULL, \n")
-	createTableCommand.WriteString("encryptedMessage char(200) NOT NULL \n")
+	createTableCommand.WriteString("SSID INT NOT NULL PRIMARY KEY, \n")
+	createTableCommand.WriteString("user_id INT NOT NULL, \n")
+	createTableCommand.WriteString("friend_id INT NOT NULL, \n")
+	createTableCommand.WriteString("private_key varchar(10000) NOT NULL, \n")
+	createTableCommand.WriteString("fingerprint varchar(10000) NOT NULL \n")
 	createTableCommand.WriteString(" );")
 	ExecuteDatabaseCommand(db, createTableCommand.String())
 }
 
-// Inserts values given into the table
-func InsertValues(db *sql.DB, username string, friendsName string, privateKey string, publicKey string, message string){
-	insertCommand := fmt.Sprintf("INSERT INTO %s VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\")", tableName, username, friendsName, privateKey, publicKey, message)
+// Creates the users table
+func SetupUsersTable(db *sql.DB) {
+	var createTableCommand bytes.Buffer
+	createTableCommand.WriteString("CREATE TABLE IF NOT EXISTS ")
+	createTableCommand.WriteString(userTableName)
+	createTableCommand.WriteString(" (\n")
+	createTableCommand.WriteString("id INT NOT NULL, \n")
+	createTableCommand.WriteString("login varchar(10000) NOT NULL, \n")
+	createTableCommand.WriteString("password varchar(10000) NOT NULL \n")
+	createTableCommand.WriteString(" );")
+	ExecuteDatabaseCommand(db, createTableCommand.String())
+}
+
+// Creates the conversations table
+func SetupConversationTable(db *sql.DB) {
+	var createTableCommand bytes.Buffer
+	createTableCommand.WriteString("CREATE TABLE IF NOT EXISTS ")
+	createTableCommand.WriteString(conversationTableName)
+	createTableCommand.WriteString(" (\n")
+	createTableCommand.WriteString("SSID INT NOT NULL, \n")
+	createTableCommand.WriteString("message varchar(10000) NOT NULL, \n")
+	createTableCommand.WriteString("timestamp varchar(30) NOT NULL, \n")
+	createTableCommand.WriteString("sent_or_received BIT NOT NULL")
+	createTableCommand.WriteString(" );")
+	ExecuteDatabaseCommand(db, createTableCommand.String())
+}
+
+func InsertIntoConversations(db *sql.DB, SSID int, message string, timestamp string, sentOrReceived int) {
+	if sentOrReceived != 0 && sentOrReceived != 1 {
+		fmt.Println("Invalid entry for sent/received - msut be 0 or 1. Instead, received a %d", sentOrReceived)
+	}
+	insertCommand := fmt.Sprintf("INSERT INTO %s VALUES (%d, \"%s\", \"%s\", %d)", conversationTableName, SSID, message, timestamp, sentOrReceived)
 	ExecuteDatabaseCommand(db, insertCommand)
+}
+func InsertIntoSessions(db *sql.DB, SSID int, userId int, friendId int, privateKey string, fingerprint string) {
+	insertCommand := fmt.Sprintf("INSERT INTO %s VALUES (%d, %d, %d, \"%s\", \"%s\")", sessionsTableName, SSID, userId, friendId, privateKey, fingerprint)
+	ExecuteDatabaseCommand(db, insertCommand)
+}
+func InsertIntoUsers(db *sql.DB, id int, login string, password string) {
+	insertCommand := fmt.Sprintf("INSERT INTO %s VALUES (%d, \"%s\", \"%s\")", userTableName, id, login, password)
+	ExecuteDatabaseCommand(db, insertCommand)
+}
+
+/*
+func QueryDatabase(db *sql.DB) {
+	query := "SELECT username, friendName FROM " + tableName;
+	ExecuteQuery(db, query)
+}
+*/
+
+// Executes the specified database command
+func ExecuteQuery(db *sql.DB, query string) {
+	results, err := db.Query(query)
+	if err != nil {
+		fmt.Printf("Failed to execute query %s: %s", query, err)
+		panic(err)
+	}
+	var (
+		username string
+		friendName string
+	)
+	for results.Next(){
+		err = results.Scan(&username, &friendName)
+		if err!= nil {
+			fmt.Printf("Failed to execute query %s: %s", query, err)
+			panic(err)
+		}
+		fmt.Printf("User: %s, Friend: %s\n", username, friendName)
+	}
+	err = results.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+func ShowTables(db *sql.DB) {
+	results, err := db.Query("SHOW TABLES")
+	if err != nil {
+		fmt.Println("Failed to display tables.")
+		panic(err)
+	}
+	var str string
+	for results.Next() {
+		err = results.Scan(&str)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Show tables: %s\n", str)
+	}
 }
