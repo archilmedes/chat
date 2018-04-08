927eb4407efced848916cda46eb3ffbda9fbc5f0
archillin <archil.shah@gmail.com>
2018-04-07 21:03:24 -0500

    Implement basic server

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"chat/server"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+const Exit = "exit\n"
 
 func main() {
-	fmt.Println("Hello World!")
+	var program server.Server
+	if err := program.Start("Archil"); err != nil {
+		log.Fatalf("main: %s", err.Error())
+	}
+	sig := make (chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		program.Shutdown()
+		os.Exit(0)
+	}()
+	for {
+		var message string
+		var err error
+		reader := bufio.NewReader(os.Stdin)
+		message, err = reader.ReadString('\n')
+		if err != nil {
+			program.Shutdown()
+			log.Fatalf("main: %s", err)
+			os.Exit(1)
+		}
+		if message == Exit {
+			program.Shutdown()
+			os.Exit(0)
+		}
+		stringSlice := strings.Fields(message)
+		if err = program.Send(stringSlice[0], strings.Join(stringSlice[1:], " ")); err != nil {
+			fmt.Errorf("main: %s", err.Error())
+		} else {
+			fmt.Println("Try again!")
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,39 +1,33 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 )
 
 const (
-	Port uint16  = 4242
+	Port uint16 = 4242
 	Network = "tcp"
 )
 
-type Server interface {
-	Start() error
-	Shutdown() error
-	Send()
-	Recv()
-}
-
-type SimpleServer struct {
-	IP string
+// Simple Server struct
+type Server struct {
+	IP, Username string
 	Listener *net.TCPListener
 }
 
-/**
- * Get public ip address.
- * Help from: https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
- */
+// Get public ip address
+// Help from: https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
 func getIp() (string, error) {
-	addrs, err := net.InterfaceAddrs()
+	addresses, err := net.InterfaceAddrs()
 	if err != nil {
 		return "", err
 	}
-	for _, i := range addrs {
-		if ipInfo, ok := i.(*net.IPNet); ok && !ipInfo.IP.IsLoopback() {
+	for _, addr := range addresses {
+		if ipInfo, ok := addr.(*net.IPNet); ok && !ipInfo.IP.IsLoopback() {
 			if ipInfo.IP.To4() != nil {
 				return ipInfo.IP.String(), nil
 			}
@@ -42,11 +36,8 @@ func getIp() (string, error) {
 	return "", errors.New("getIp: cannot find public ip address")
 }
 
-/**
- * Setup listener for server.
- */
-func setupServer() (*net.TCPListener, error) {
-	address := fmt.Sprintf("localhost:%d", Port)
+// Setup listener for server
+func setupServer(address string) (*net.TCPListener, error) {
 	tcpAddr, err := net.ResolveTCPAddr(Network, address)
 	if err != nil {
 		return nil, err
@@ -54,31 +45,68 @@ func setupServer() (*net.TCPListener, error) {
 	return net.ListenTCP(Network, tcpAddr)
 }
 
-/**
- * Start up server.
- */
-func (s *SimpleServer) Start() error {
+// Handle receiving messages from a TCPConn
+func handleConnection(conn *net.TCPConn) {
+	defer conn.Close()
+	decoder := json.NewDecoder(conn)
+	var msg Message
+	if err := decoder.Decode(&msg); err != nil {
+		log.Panicf("handleConnection: %s", err.Error())
+	}
+}
+
+// Function that continuously polls for new messages being sent to the server
+func receive(listener *net.TCPListener) {
+	for {
+		if conn, err := (*listener).AcceptTCP(); err == nil {
+			go handleConnection(conn)
+		}
+	}
+}
+
+// Start up server
+func (s *Server) Start(username string) error {
+	log.Println("Launching Server...")
+	(*s).Username = username
 	var err error
-	fmt.Println("Launching Server...")
-	(*s).IP, err = getIp()
-	if err != nil {
+	if (*s).IP, err = getIp(); err != nil {
 		return err
 	}
-	(*s).Listener, err = setupServer()
-	if err != nil {
+	ipAddr := fmt.Sprintf("%s:%d", (*s).IP, Port)
+	if (*s).Listener, err = setupServer(ipAddr); err != nil {
 		return err
 	}
-	fmt.Printf("Listening on: '%s:%d'", (*s).IP, Port)
+	go receive((*s).Listener)
+	log.Printf("Listening on: '%s:%d'", (*s).IP, Port)
 	return nil
 }
 
-/**
- * End server connection.
- */
-func (s *SimpleServer) Shutdown() error {
+// End server connection
+func (s *Server) Shutdown() error {
+	log.Println("Shutting Down Server...")
 	return (*s).Listener.Close()
 }
 
-/**
- * Send
- */
+func initDialer(address string) (*net.TCPConn, error) {
+	tcpAddr, err := net.ResolveTCPAddr(Network, address)
+	if err != nil {
+		return nil, err
+	}
+	return net.DialTCP(Network, nil, tcpAddr)
+}
+
+// Send a message to another Server
+func (s *Server) Send(address string, message string) error  {
+	fmt.Printf("%s %s\n", address, message)
+	dialer, err := initDialer(address)
+	if err != nil {
+		return err
+	}
+	var msg Message
+	msg.Init((*s).Username, message)
+	encoder := json.NewEncoder(dialer)
+	if err = encoder.Encode(&msg); err != nil {
+		return err
+	}
+	return nil
+}
