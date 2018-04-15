f2344c29ae73b23aec58ff8c4852ccc35012625a
archillin <archil.shah@gmail.com>
2018-04-15 15:36:28 -0500

    Correct working directory

--- db/db_setup.sh
+++ db/db_setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
 if [ -z "$2" ]; then
-	mysql -u "$1" < "db/db_setup.sql"
+	mysql -u "$1" < "db_setup.sql"
 else
-	mysql -u "$1" -p "$2" < "db/db_setup.sql"
+	mysql -u "$1" -p "$2" < "db_setup.sql"
 fi
