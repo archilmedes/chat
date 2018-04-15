5b754a13bb2a3a21eef08c48759df8f954cc8524
archillin <archil.shah@gmail.com>
2018-04-15 15:09:46 -0500

    Fix bash script to work on Unix

--- db/db_setup.sh
+++ db/db_setup.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-if [ "$2" = "" ]; then
-    mysql -u "$1" < "db_setup.sql"
+#!/bin/bash
+
+if [ -z "$2" ]; then
+	mysql -u "$1" < "db/db_setup.sql"
 else
-    mysql -u "$1" -p"$2" < "db_setup.sql"
-fi;
+	mysql -u "$1" -p "$2" < "db/db_setup.sql"
+fi
