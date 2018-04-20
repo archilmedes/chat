86fa9d2f73411f8f61d7bf796b24f7b6b29093bd
Andrew Doherty <adohert2@illinois.edu>
2018-04-20 16:53:28 -0500

    Fixed stale function

--- db/conversations_test.go
+++ db/conversations_test.go
@@ -7,7 +7,6 @@ import (
 
 func ConversationsTest(t *testing.T) {
 	ConversationsTestID(t)
-	ConversationsTestSSID(t)
 }
 
 func ConversationsTestID(t *testing.T) {
@@ -18,17 +17,4 @@ func ConversationsTestID(t *testing.T) {
 	assert.Equal(t, 34, int(conversations[2].Session.SSID))
 	assert.Equal(t, "2018-04-10 12:30:08.222222", conversations[0].Message.Timestamp)
 	assert.Equal(t, "When are we playing Fortnite?", conversations[2].Message.Text)
-}
-
-func ConversationsTestSSID(t *testing.T) {
-	conversations := GetConversationSSID(34)
-	assert.Equal(t, 3, len(conversations))
-	assert.Equal(t, 34, int(conversations[0].Session.SSID))
-	assert.Equal(t, "plain", conversations[0].Session.ProtocolType)
-	assert.Equal(t, "I almost made my Mac a brick", conversations[1].Message.Text)
-	assert.Equal(t, 34, int(conversations[2].Session.SSID))
-	assert.Equal(t, "karateAMD", conversations[2].Session.Username)
-	assert.Equal(t, "10.192.345.987", conversations[2].Session.FriendMac)
-	assert.Equal(t, "2018-04-08 17:59:02.777777", conversations[2].Message.Timestamp)
-	assert.Equal(t, "plain", conversations[2].Session.ProtocolType)
 }
