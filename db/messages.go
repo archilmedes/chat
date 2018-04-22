c0961de1cbde959a9db4402c88de7436adbeef09
Andrew Doherty <adohert2@illinois.edu>
2018-04-21 21:58:29 -0500

    Friends (#6)
    * Added friends table
    
    * Fixed stale function
    
    * Fixed PR comments
    
    * Removed display name functions
    
    * Added tests for friends
    
    * Changed SSID to uint64
    
    * Fixed uint64 and removed unnecessary functions discussed in PR
    
    * Made sizes of data fields consistent
    
    * Added logging for DB

--- db/messages.go
+++ db/messages.go
@@ -12,12 +12,13 @@ const (
 
 // Stores a DB Message
 type DBMessage struct {
-	SSID, SentOrReceived int
+	SSID              uint64
+	SentOrReceived    int
 	Text, Timestamp   string
 }
 
 // Inserts a message into the messages table
-func InsertMessage(SSID int, message string, timestamp string, sentOrReceived int) bool {
+func InsertMessage(SSID uint64, message string, timestamp string, sentOrReceived int) bool {
 	log.Println("Inserting data into messages...")
 	if sentOrReceived != Sent && sentOrReceived != Received {
 		fmt.Printf("Invalid entry for sent/received - msut be 0 or 1. Instead, received a %d", sentOrReceived)
@@ -27,7 +28,7 @@ func InsertMessage(SSID int, message string, timestamp string, sentOrReceived in
 }
 
 // Removes a message from the messages table
-func DeleteMessage(SSID int, message string, timestamp string, sentOrReceived int) bool {
+func DeleteMessage(SSID uint64, message string, timestamp string, sentOrReceived int) bool {
 	deleteCommand := fmt.Sprintf("DELETE FROM %s WHERE SSID=%d AND message=\"%s\" AND message_timestamp=\"%s\" AND sent_or_received=%d", messagesTableName, SSID, message, timestamp, sentOrReceived)
 	return ExecuteChangeCommand(deleteCommand, "Failed to delete message")
 }
@@ -43,22 +44,21 @@ func QueryMessages() []DBMessage {
 func ExecuteMessagesQuery(query string) []DBMessage {
 	results, err := DB.Query(query)
 	if err != nil {
-		fmt.Printf("Failed to execute query %s: %s", query, err)
-		panic(err)
+		log.Panicf("Failed to execute %s on messages table: %s", query, err)
 	}
 	var messages []DBMessage
 	msg := DBMessage{}
 	for results.Next() {
 		err = results.Scan(&msg.SSID, &msg.Text, &msg.Timestamp, &msg.SentOrReceived)
 		if err != nil {
-			fmt.Printf("Failed to parse results %s: %s", query, err)
+			log.Panicf("Failed to parse results from messages with query: %s;  %s", query, err)
 			panic(err)
 		}
 		messages = append(messages, msg)
 	}
 	err = results.Err()
 	if err != nil {
-		log.Fatal(err)
+		log.Panicf("Failed to get results from conversations query: ", err)
 	}
 	return messages
 }
